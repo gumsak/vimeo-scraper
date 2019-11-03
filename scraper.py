77ed36e8f123e5dd28ae680fdae25695d430d1b4
MB <gumsak@users.noreply.github.com>
2019-11-03 13:54:03 +0100

    replacing requests with scrapy

--- scraper.py
+++ scraper.py
@@ -1,18 +1,40 @@
 # Porjet scraper Vimeo
 
-# import lib requests
+# import libs
 import requests
+import scrapy
 
-# get from the given address
-response = requests.get("https://www.google.fr/")
+# class to represent a video available online
+class Video:
+    #constructor
+    def __init__(self, name, url):
+        # name of the video
+        self.name = name
+        # URL of the video
+        self.url = url
 
-print(response.status_code)
+    def setName(self):
+        
 
-ploads = {'things':2, 'total':25}
+# create one object to retrieve a video
+vid1 = Video("et","at")
 
-r = requests.get('https://httpbin.org/get', params = ploads)
+print(vid1.url)
+    
+# GET from the given address
+#response = requests.get("https://www.google.fr/")
 
-print(r.text)
-print(r.url)
+#print(response.status_code)
+
+#ploads = {'things':2, 'total':25}
+
+#r = requests.get('https://httpbin.org/get', params = ploads)
+
+#print(r.text)
+#print(r.url)
+
+# Init the crawler
+
+
 
 #print(response.text)
