3ab93d59da63400afa2f2013c9b929a716978a56
MB <gumsak@users.noreply.github.com>
2020-01-24 18:53:07 +0100

    updated the segment decoder file's functions and comments

--- src/segments_decoder.py
+++ src/segments_decoder.py
@@ -13,8 +13,34 @@ import subprocess
 def cat_segments(segments_directory, segments_extension,
                  has_init_segment, output_name, output_extension, 
                  init_segment_name = None, nb_segments = 0, segments_list=None,
-                 segment_patern = 'segment-{}.m4s'):
+                 segment_pattern = 'segment-{}.m4s'):
+    """
+    Concatenate multiple files together.
+
+    Parameters
+    ----------
+    segments_directory (string): directory of the files to concatenate
     
+    segments_extension (string): extension of the files to concatenate
+    
+    has_init_segment (boolean): define whether or not an initial file/segment
+    has to be treated first
+    
+    output_name (string): name of the file produced by the concatenation
+    
+    output_extension (string): extension of the produced file
+    
+    init_segment_name (string): (optional) name of the initial file/segment. 
+    The default is None.
+    
+    nb_segments (int): (optional) amount of files to handle. The default is 0.
+        
+    segments_list [NOT IMPLEMENTED] : (optional) list of files to handle. The 
+    default is None.
+        
+    segment_pattern (string): (optional) patern/name of the files to handle. 
+    The default is 'segment-{}.m4s'.
+    """
     output_file = segments_directory + output_name + output_extension
             
     out = open(output_file, "ab")
@@ -30,6 +56,7 @@ def cat_segments(segments_directory, segments_extension,
         #os.system('cat ' +  + ' *' + segments_extension + '>>')
 
     """ref: https://www.geeksforgeeks.org/python-program-to-merge-two-files-into-a-third-file/"""
+    
     '''
     with open(segments_directory + output_file, "ab") as outfile:
         for file_segment in os.listdir(segments_directory):
@@ -46,7 +73,7 @@ def cat_segments(segments_directory, segments_extension,
     #m4s_file = open(segments_directory + segments_file, "a")
     
     for i in range (0, nb_segments):
-        file_to_open = segment_patern.format(str(i+1))
+        file_to_open = segment_pattern.format(str(i+1))
         full_path = os.path.join(segments_directory, file_to_open)
         
         #if full_path.endswith(segments_extension):
@@ -92,12 +119,13 @@ def encode_mp4(input_file, output_file):
     
 def encode_mp3(input_file, output_file):
     """
-        
-    input_file : TYPE
-    DESCRIPTION.
-    output_file : TYPE
-    DESCRIPTION.
+    Encode a binary sound file to produce an mp3 file 
+    
+    input_file : (binary) audio file to encode
+    
+    output_file : name of the mp3 file generated (add file extension in name)
     """
+    
     """
     subprocess.call(['ffmpeg',
                          '-i',
@@ -124,14 +152,11 @@ def encode_mp3(input_file, output_file):
                          output_file])
     
 def delete_files(extension, directory=''):
-    """delete specific files from the given directory
-        
-    directory : STRING
-    (optional) path of the directory to check
-    extension : STRING
-    extension of the files to delete
-            
-    Return: None
+    """Delete specific files from the given directory
+    
+    extension (string): extension of the files to delete
+    
+    directory (string): (optional) path of the directory to check
     """
 
     for file in os.listdir(directory):
@@ -144,7 +169,7 @@ def delete_files(extension, directory=''):
                 print("Error while removing file")
     
 def combine_files(video_file, audio_file, output_file):
-    """join an audio file and a video file to create a new file"""
+    """Join an audio file & a video file to create a new video file with sound"""
     
     subprocess.call(['ffmpeg',
                      '-y',
