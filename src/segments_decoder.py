f053cdbcf9ed9eea5a26005edcd564968eb83bc4
MB <gumsak@users.noreply.github.com>
2020-01-26 19:09:53 +0100

    cleaning segment_decoder file, removing commented code, unused libraries, etc

--- src/segments_decoder.py
+++ src/segments_decoder.py
@@ -12,8 +12,6 @@ from __future__ import print_function
 import ffmpy
 import base64
 import os
-import subprocess
-import glob
 import fnmatch
 
 def cat_segments(segments_directory, segments_extension,
@@ -60,17 +58,6 @@ def cat_segments(segments_directory, segments_extension,
         f.close()
         
         #os.system('cat ' +  + ' *' + segments_extension + '>>')
-
-    """ref: https://www.geeksforgeeks.org/python-program-to-merge-two-files-into-a-third-file/"""
-    
-    '''
-    with open(segments_directory + output_file, "ab") as outfile:
-        for file_segment in os.listdir(segments_directory):
-            if file_segment.endswith(segments_extension):
-                with open(file_segment, "rb") as infile:
-                    
-                    outfile.write(infile.read())
-    '''
     
     #cat all the segments in order
     #ref: https://stackoverflow.com/a/58575327
@@ -83,24 +70,7 @@ def cat_segments(segments_directory, segments_extension,
         with open(full_path, "rb") as infile:
             out.write(infile.read())
             infile.close()
-    
-    """
-    make sure to get the correct path
-    ref:
-        https://stackoverflow.com/a/53296655
-    & 
-        https://stackoverflow.com/a/26065676
-    """
-    """
-    for file_segment in os.listdir(segments_directory):
-        full_path = os.path.join(segments_directory, file_segment)
-        
-        if full_path.endswith(segments_extension):
-            print(full_path)
-            with open(full_path, "rb") as infile:
-                out.write(infile.read())
-                infile.close()
-    """       
+         
     out.close()
     
 def cat_files(segments_directory, init_file, segment_file,
@@ -143,15 +113,7 @@ def encode_mp4(input_file, output_file):
     
     file (string): file to encode    
     """
-    
-    '''
-    subprocess.call(['ffmpeg',
-                     '-y',
-                     '-i',
-                     input_file, 
-                     '-c', 'copy',
-                     output_file])
-    '''
+
     ff = ffmpy.FFmpeg(inputs= {input_file:None},
                  outputs= {output_file:'-c copy'}, 
                  global_options = ('-y')
@@ -176,33 +138,6 @@ def encode_mp3(input_file, output_file, ffmpeg_path= 'ffmpeg'):
                  global_options = ('-y')
                  )
     ff.run()
-    
-    """
-    subprocess.call(['ffmpeg',
-                         '-i',
-                         input_file,
-                         '-codec:a',
-                         'libmp3lame',
-                         '-qscale:a',
-                         '2',
-                         output_file])
-    """
-    
-    """
-    #ref https://stackoverflow.com/a/12952172
-    subprocess.call(['/usr/bin/ffmpeg',
-                     '-y',
-                         '-i',
-                         input_file,
-                         '-vn',
-                         '-ar',
-                         '44100',
-                         '-ac',
-                         '2',
-                         '-b:a',
-                         '192k',
-                         output_file])
-    """
     
 def delete_files(directory=''):
     """Delete specific files from the given directory
@@ -231,18 +166,6 @@ def delete_files(directory=''):
                                     
 def combine_files(video_file, audio_file, output_file):
     """Join an audio file & a video file to create a new video file with sound"""
-    
-    """
-    subprocess.call(['ffmpeg',
-                     '-y',
-                     '-i',
-                     video_file,
-                     '-i',
-                     audio_file,
-                     '-c',
-                     'copy',
-                     output_file])
-    """
 
     ff = ffmpy.FFmpeg(inputs= {video_file:None, audio_file:None},
                  outputs= {output_file:'-c copy'}, 
